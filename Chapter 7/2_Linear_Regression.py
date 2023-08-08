8cb69a0430c02b76be63c45b2bb8d0027dd61288
Sofien KAABAR <86428036+sofienkaabar@users.noreply.github.com>
2023-08-08 15:55:43 +0200

    Add files via upload

---
+++ Chapter 7/2_Linear_Regression.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from master_function import data_preprocessing, mass_import
+from master_function import plot_train_test_values, calculate_accuracy, model_bias
+from sklearn.metrics import mean_squared_error
+
+# importing the time series
+data = np.diff(mass_import(0, 'H1')[:, 3])
+
+# Setting the hyperparameters
+num_lags = 500
+train_test_split = 0.80
+
+# Creating the training and test sets
+x_train, y_train, x_test, y_test = data_preprocessing(data, num_lags, train_test_split)
+
+# Fitting the model
+model = LinearRegression()
+model.fit(x_train, y_train)
+
+# Predicting in-sample
+y_predicted_train = np.reshape(model.predict(x_train), (-1, 1))
+
+# Predicting out-of-sample
+y_predicted = np.reshape(model.predict(x_test), (-1, 1))
+
+# plotting
+plot_train_test_values(100, 50, y_train, y_test, y_predicted)
+
+# Performance evaluation
+print('---')
+print('Accuracy Train = ', round(calculate_accuracy(y_predicted_train, y_train), 2), '%')
+print('Accuracy Test = ', round(calculate_accuracy(y_predicted, y_test), 2), '%')
+print('RMSE Train = ', round(np.sqrt(mean_squared_error(y_predicted_train, y_train)), 10))
+print('RMSE Test = ', round(np.sqrt(mean_squared_error(y_predicted, y_test)), 10))
+print('Correlation In-Sample Predicted/Train = ', round(np.corrcoef(np.reshape(y_predicted_train, (-1)), y_train)[0][1], 3))
+print('Correlation Out-of-Sample Predicted/Test = ', round(np.corrcoef(np.reshape(y_predicted, (-1)), np.reshape(y_test, (-1)))[0][1], 3))
+print('Model Bias = ', round(model_bias(y_predicted), 2))
+print('---')
